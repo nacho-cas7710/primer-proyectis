306ad69aed62ad98e5e9347a474ed44d16e831f7
nacho <ignaciocastillo.3423@gmail.com>
2024-05-08 17:32:42 -0300

    primer proyecto

---
+++ VARIABLES.py
@@ -0,0 +1,71 @@
+#nuemro1=float(6.3)
+#numeoro2=int(10)
+#resultado=nuemro1%numeoro2
+#print(resultado)
+
+#numero2=float(2+3.3)
+#print(numero2)
+
+#numero3=str("9gsgwyg6wywyy8498")
+#print(numero3)
+
+numero4=bool(2==2)
+print(numero4)
+
+numero3=type(1.6)
+print(numero3)
+
+nombre=input("ingresame un numero:")
+
+print("mi telefono es:",nombre)
+
+#porcentaje
+#numero=int(input("ingrese mi numero:"))
+#resultado=numero*15
+#total=resultado/100
+#print(total)
+
+
+
+
+
+#ejercicios
+#nombre="juan"
+#print(nombre)
+#nombre2="castillo"
+#print(nombre2)
+
+nombre=input("ingrese su nombre:")
+print("Hola",nombre,", ¿te gustaría aprender a programar?")
+
+#numero=int(input("ingrese numero:"))
+#resultado1=numero - numero*15//100
+#print("el numero con el 15% menos es:",resultado1)
+
+
+
+
+# Solicitar al usuario que ingrese tres números
+numero1 = float(input("Por favor, ingresa el primer número: "))
+numero2 = float(input("Por favor, ingresa el segundo número: "))
+numero3 = float(input("Por favor, ingresa el tercer número: "))
+
+# Calcular el promedio de los tres números
+promedio = (numero1 + numero2 + numero3) // 3
+
+# Mostrar el promedio en pantalla
+print("El promedio de los tres números es:", promedio)
+
+
+
+# Solicitar al usuario que ingrese su nombre
+nombre = input("Por favor, ingresa tu nombre: ")
+
+# Solicitar al usuario que ingrese su edad
+edad = int(input("Por favor, ingresa tu edad: "))
+
+# Calcular el año en que cumplirá 100 años
+anio_cumple_100 = (100 - edad) + 2024
+
+# Mostrar un mensaje con el año en que cumplirá 100 años
+print("Hola,", nombre + ",", "cumplirás 100 años en el año", anio_cumple_100)
